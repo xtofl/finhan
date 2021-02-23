28f683b44eabd3d192527cfc2fd61c81e81eec09
Kristoffel Pirard <kristoffel.pirard@midiagnostics.com>
2021-02-23 17:37:52 +0100

    Support multiple input files

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 from datetime import timedelta
+from itertools import chain
 from pathlib import Path
 from argparse import ArgumentParser
+from typing import Iterator, Iterable
+
 import numpy as np
 from matplotlib import pyplot
 import adapters.bepost
@@ -36,19 +39,27 @@ def plot(dates, numbers):
     pyplot.legend()
 
 
+def read_lines(filename: str) -> Iterator[str]:
+    with Path(filename).open() as f:
+        return f.readlines()
+
+
 def main():
     parser = ArgumentParser()
-    parser.add_argument('data_path', type=str)
+    parser.add_argument('data_path', type=str, nargs='*')
     options = parser.parse_args()
 
-    with Path(options.data_path).open() as f:
-        transactions = sorted(
-            tuple(adapters.bepost.from_lines(f.readlines())),
-            key=lambda t: t.date,
-            reverse=True)
-        dates = tuple(t.date for t in transactions)
-        numbers = tuple(t.amount for t in transactions)
-        plot(dates, numbers)
+    line_lists = map(read_lines, options.data_path)
+    transaction_lists = map(adapters.bepost.from_lines, line_lists)
+
+    transactions = sorted(
+        chain(*transaction_lists),
+        key=lambda t: t.date,
+        reverse=True)
+
+    dates = tuple(t.date for t in transactions)
+    numbers = tuple(t.amount for t in transactions)
+    plot(dates, numbers)
     pyplot.show()
 
 
