95af895600de4f6ede7ba6d08fa7cab34c53c169
Kristoffel Pirard <kristoffel.pirard@midiagnostics.com>
2021-02-27 15:24:10 +0100

    Use Safe yaml loader
    YAMLLoadWarning: calling yaml.load() without Loader=... is deprecated, as the default Loader is unsafe. Please read https://msg.pyyaml.org/load for full details.

--- src/finhan/account.py
+++ src/finhan/account.py
@@ -8,5 +8,6 @@ Balance = float
 
 def read_balance(filename: Path) -> Dict[AccountId, Balance]:
     with filename.open() as f:
-        return {AccountId(a): Balance(b) for a, b in yaml.load(f).items()}
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+    return {AccountId(a): Balance(b) for a, b in config.items()}
 
