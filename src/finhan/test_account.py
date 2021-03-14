7b54bd7d748a2c9112c93513a1d3977d6169ff64
Kristoffel Pirard <kristoffel.pirard@midiagnostics.com>
2021-03-14 14:44:08 +0100

    test balance reading from file

--- src/finhan/test_account.py
+++ src/finhan/test_account.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
-from finhan.account import apply_balance, joint_account_transactions, Account
+import pytest
+
+from finhan.account import apply_balance, joint_account_transactions, Account, \
+    read_balance
 from finhan.adapters.bepost import Transaction
 
 
@@ -34,3 +37,46 @@ def test_grand_total_ends_in_grand_balance():
     assert tuple(dates) == (d1, d2, d3)
     assert numbers[-1] == 1000
     assert tuple(numbers) == (1050, 1050, 1000)
+
+
+def test_balance_can_be_read(tmp_path):
+    yaml = r"""
+schema: "v1"
+last updated: 2021-03-13
+accounts:
+        -
+                id: BE123456789
+                name: daily
+                balance: 201.11
+        -
+                id: BE654987321
+                name: backup
+                balance: 1735.91
+        -
+                id: BE489156489
+                name: savings
+                balance: 9134.68
+"""
+    balance_file = tmp_path / 'b.yml'
+    with open(balance_file, 'w') as f:
+        f.write(yaml)
+
+    balance = read_balance(balance_file)
+    assert set(('BE123456789', 'BE654987321', 'BE489156489')) == set(
+        balance.keys())
+
+    assert balance['BE123456789'].name == 'daily'
+    assert balance['BE123456789'].balance == 201.11
+
+
+def test_read_balance_raises_for_unknown_schema(tmp_path):
+    yaml = r"""
+    schema: "vWHAT?"
+    last updated: 2021-03-13
+"""
+    balance_file = tmp_path / 'b.yml'
+    with open(balance_file, 'w') as f:
+        f.write(yaml)
+
+    with pytest.raises(Exception):
+        read_balance(balance_file)
