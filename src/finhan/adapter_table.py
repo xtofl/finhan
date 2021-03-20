7a87711ac50ee9c6aafaff31e7884654ac57451d
Kristoffel Pirard <kristoffel.pirard@midiagnostics.com>
2021-03-20 15:32:21 +0100

    Add 'finhan-table' script

--- src/finhan/adapter_table.py
+++ src/finhan/adapter_table.py
@@ -1,5 +1,9 @@
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
 from typing import Iterable
 
+from finhan.account import read_account_transactions
 from finhan.transaction import Transaction
 
 
@@ -14,3 +18,34 @@ def _to_row(transaction: Transaction) -> str:
 
 def generate_from(title: str, transactions: Iterable[Transaction]):
     return title + "\n" + "\n".join(map(_to_row, transactions))
+
+
+class AccountNotFound(Exception):
+    def __init__(self, accounts):
+        self.accounts = accounts
+        super().__init__()
+
+
+def generate_from_files(account_id: str, files: Iterable[Path]):
+    title = "account: " + account_id
+    transactions_per_account = read_account_transactions(files)
+    try:
+        transactions = transactions_per_account[account_id]
+    except KeyError:
+        raise AccountNotFound(transactions_per_account.keys())
+    return generate_from(title, transactions)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--account", required=True)
+    parser.add_argument("files", nargs="*")
+    options = parser.parse_args()
+    try:
+        print(generate_from_files(options.account, map(Path, options.files)))
+    except AccountNotFound as a:
+        print(f"account not found; valid accounts: [{a.accounts}]")
+
+
+if __name__ == "__main__":
+    main()
